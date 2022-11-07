9a9cbab84b51326f2d452e93c5ccd68ebb18b7c3
Joe Friel <joefriel@Ramy-MacBook-Pro.local>
2022-11-07 13:58:45 +0200

    add translation files

---
+++ client/src/pages/CoLHomePage/LandingContent.js
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import { message } from 'antd';
+
+import { Typography as T } from '../../components';
+import { LandingPage } from '../../api-calls';
+import { t } from '../../helpers';
+import { useLang } from '../../context/lang';
+import { usePublicOrg } from '../../context/public-org';
+
+import * as S from './style';
+
+const formatText = (text) => {
+  if (!text) return '';
+  const arr = text.split(/\. |! |\? /gm);
+  const firstSentence = arr[0];
+  if (!arr[1]) return <T.H2 color="primaryMain">{firstSentence}</T.H2>;
+
+  const remainder = arr.slice(1).join(' ');
+  return (
+    <>
+      <T.H2 color="primaryMain" mb="2" mr="2" mt="2">
+        {firstSentence}! <S.Span>{remainder}</S.Span>
+      </T.H2>
+    </>
+  );
+};
+
+const LandingContent = ({ uniqueSlug }) => {
+  const { lang } = useLang();
+  const { publicOrg } = usePublicOrg();
+  const [landingContent, setLandingContent] = useState({});
+  const [fetchError, setFetchError] = useState('');
+
+  useEffect(() => {
+    let mounted = true;
+    async function fetchData() {
+      const hideMessage = message.loading('Loading...');
+      const { data, error } = await LandingPage.getLandingPageContent({});
+      if (mounted) {
+        if (error) {
+          setFetchError(t(`generalError`, lang));
+        } else {
+          setLandingContent(data);
+        }
+        hideMessage();
+      }
+    }
+
+    fetchData();
+    return () => {
+      mounted = false;
+    };
+  }, [lang]);
+
+  return (
+    <>
+      <S.PageHead
+        showBGImage={!uniqueSlug || (publicOrg && Number(publicOrg?.id) === 1)}
+      >
+        <S.HeaderText>
+          {fetchError ? (
+            <T.P color="error">{fetchError}</T.P>
+          ) : (
+            <T.H2 weight="bold" color="white">
+              {landingContent.headline}
+            </T.H2>
+          )}
+        </S.HeaderText>
+      </S.PageHead>
+      <S.Section>
+        {formatText(landingContent.subtitle)}{' '}
+        <S.StyledText mb="3">{landingContent.instructions}</S.StyledText>
+      </S.Section>
+    </>
+  );
+};
+
+export default LandingContent;
---
+++ client/src/pages/CoLHomePage/index.js
@@ -0,0 +1,233 @@
+import { useState, useEffect, useRef } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import ReactGA from 'react-ga';
+import Step from '../../components/Steps';
+import { Typography as T } from '../../components';
+import { t } from '../../helpers';
+import { useLang } from '../../context/lang';
+import { useSteps } from '../../context/steps';
+import { navRoutes as n } from '../../constants';
+import LandingContent from './LandingContent';
+import { stageTypes } from '../../constants/data-types';
+
+import Icon from '../../components/Icon';
+import HelpButton from '../../components/HelpButton';
+import TextWithIcon from '../../components/TextWithIcon';
+import { Translation } from '../../api-calls';
+
+import * as S from './style';
+
+const afterClaimContent = {
+  title: {
+    completed: 'You’re all done!',
+    notCompleted: `What should I do once I am granted Universal Credit?`,
+  },
+  text: {
+    completed: `Got your Universal Credit? Great news! Check out these steps on what to do next:`,
+    notCompleted: `Once you’ve completed your claim there are few additional steps you can take. Open this when you’ve completed the above steps`,
+  },
+};
+
+const Home = () => {
+  const { lang } = useLang();
+  const {
+    steps,
+    justCompletedId,
+    setJustCompletedId,
+    loadingSteps,
+    stepsObj,
+  } = useSteps();
+
+  // Translation
+
+  const [showAfterClaim, setShowAfterClaim] = useState(false);
+
+  const currentStep = steps.find(
+    (step) => !step.isCompleted && step.stage !== stageTypes.BEFORE_CLAIMING
+  );
+  const currentStepRef = useRef();
+  const { org } = useParams();
+  const history = useHistory();
+
+  const completedClaim = currentStep?.stage === stageTypes.AFTER_CLAIMING;
+
+  const getStepStatus = (step, i) => {
+    const isCurrentStep = currentStep && step.id === currentStep.id;
+    const variant = step.isCompleted
+      ? 'neutral'
+      : isCurrentStep
+      ? 'primary'
+      : 'secondary';
+    const isJustCompletedOne = step.id === justCompletedId;
+    // To only add ref to the currentStep
+    let currentRef = isCurrentStep ? currentStepRef : null;
+    if (i === steps.length - 1 && step.isCompleted) {
+      currentRef = currentStepRef;
+    }
+
+    return { variant, currentRef, isJustCompletedOne, isCurrentStep };
+  };
+
+  const decideRoute = (step) =>
+    org
+      ? n.STEPS.STEP_ORG.replace(':id', step.id).replace(':org', org)
+      : n.STEPS.STEP.replace(':id', step.id);
+
+  useEffect(() => {
+    if (currentStepRef.current && justCompletedId) {
+      currentStepRef.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      ReactGA.event({
+        category: 'Completed step',
+        action: currentStep?.title,
+      });
+
+      if (completedClaim) {
+        ReactGA.event({
+          category: 'Completed claim',
+          action: 'Completed claim',
+        });
+      }
+    }
+  }, [justCompletedId]);
+
+  useEffect(() => {
+    if (org) {
+      history.push(n.GENERAL.HOME_ORG.replace(':org', org));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [org]);
+
+  return (
+    <>
+      <LandingContent uniqueSlug={org} />
+
+      {/* BEFORE CLAIMING */}
+      {stepsObj.BEFORE_CLAIMING?.map((step, i) => {
+        const { variant, currentRef, isJustCompletedOne } = getStepStatus(
+          step,
+          i
+        );
+
+        return (
+          <Step
+            key={step.id}
+            title={step.title}
+            description={step.description}
+            content={t(`${step.name}.subtitle`, lang)}
+            isCompleted={step.isCompleted}
+            variant={variant}
+            direction={i % 2 === 0 ? 'left' : 'right'}
+            mt="7"
+            isJustCompletedOne={isJustCompletedOne}
+            to={decideRoute(step)}
+            ref={currentRef}
+            isOptional={step.isOptional}
+            handleClick={() => {
+              setJustCompletedId('');
+            }}
+            loadingSteps={loadingSteps}
+          />
+        );
+      })}
+
+      {/* CLAIMING */}
+      {stepsObj.CLAIMING?.map((step, i) => {
+        const { variant, currentRef, isJustCompletedOne } = getStepStatus(
+          step,
+          i
+        );
+
+        return (
+          <Step
+            key={step.id}
+            title={step.title}
+            description={step.description}
+            content={t(`${step.name}.subtitle`, lang)}
+            isCompleted={step.isCompleted}
+            variant={variant}
+            direction={i % 2 === 0 ? 'left' : 'right'}
+            mt="7"
+            isJustCompletedOne={isJustCompletedOne}
+            to={decideRoute(step)}
+            ref={currentRef}
+            isOptional={step.isOptional}
+            handleClick={() => {
+              setJustCompletedId('');
+            }}
+            loadingSteps={loadingSteps}
+          />
+        );
+      })}
+
+      {/* AFTER CLAIMING */}
+      <S.Section mt="7">
+        <Icon icon="flag" color="primaryMain" mt="6" mb="5" mbM="0" mtM="5" />
+        <T.H2 color="neutralMain" mb="1">
+          {
+            afterClaimContent.title[
+              completedClaim ? 'completed' : 'notCompleted'
+            ]
+          }
+        </T.H2>
+        <S.StyledText>
+          {
+            afterClaimContent.text[
+              completedClaim ? 'completed' : 'notCompleted'
+            ]
+          }
+        </S.StyledText>
+        {!completedClaim && !showAfterClaim && (
+          <S.Container mt="4">
+            <TextWithIcon
+              icon="bulletArrow"
+              iconColor="primaryMain"
+              isButton
+              handleClick={() => setShowAfterClaim(true)}
+              text="View steps"
+              jc="flex-start"
+              weight="500"
+            />
+          </S.Container>
+        )}
+      </S.Section>
+
+      {(completedClaim || showAfterClaim) &&
+        stepsObj.AFTER_CLAIMING?.map((step, i) => {
+          const { variant, currentRef, isJustCompletedOne } = getStepStatus(
+            step,
+            i
+          );
+
+          return (
+            <Step
+              key={step.id}
+              title={step.title}
+              description={step.description}
+              content={t(`${step.name}.subtitle`, lang)}
+              isCompleted={step.isCompleted}
+              variant={variant}
+              direction={i % 2 === 0 ? 'left' : 'right'}
+              mt="7"
+              isJustCompletedOne={isJustCompletedOne}
+              to={decideRoute(step)}
+              ref={currentRef}
+              isOptional={step.isOptional}
+              handleClick={() => {
+                setJustCompletedId('');
+              }}
+              loadingSteps={loadingSteps}
+            />
+          );
+        })}
+      <HelpButton />
+    </>
+  );
+};
+
+export default Home;
