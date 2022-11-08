5e64242a86a3d77376efc89435e5a57655da5ec2
fadi <fadeomar2015@gmail.com>
2022-11-08 15:31:07 +0200

    fix svg props

--- client/src/components/Icon/icons/Calculator.js
+++ client/src/components/Icon/icons/Calculator.js
@@ -10,65 +10,65 @@ const Calculator = ({ width, height, color, ...props }) => (
     <path
       d="M70.1125 4.25H12.8875C7.97907 4.25 4 8.22907 4 13.1375V92.8625C4 97.7709 7.97907 101.75 12.8875 101.75H70.1125C75.0209 101.75 79 97.7709 79 92.8625V13.1375C79 8.22907 75.0209 4.25 70.1125 4.25Z"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M59.575 19.25H23.425C20.9811 19.25 19 21.2311 19 23.675V37.325C19 39.7689 20.9811 41.75 23.425 41.75H59.575C62.0189 41.75 64 39.7689 64 37.325V23.675C64 21.2311 62.0189 19.25 59.575 19.25Z"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M64 56.75V86.75"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M52.75 56.75H41.5"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M30.25 56.75H19"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M52.75 71.75H41.5"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M30.25 71.75H19"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M52.75 86.75H41.5"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M30.25 86.75H19"
       stroke="#1A202B"
-      stroke-width="7.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="7.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
