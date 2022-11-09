b0840042acd3d6d24083bf2847008127deed6752
fadi <fadeomar2015@gmail.com>
2022-11-09 14:11:25 +0200

    update typography and colors

--- client/src/theme/colors.js
+++ client/src/theme/colors.js
@@ -11,6 +11,7 @@ export default {
   secondaryMain: formatColor(secondaryMain),
   secondaryMid: formatColor(setColor('secondary', secondaryMain).mid),
   secondaryLight: formatColor(setColor('secondary', secondaryMain).light),
+  tertiaryMain: 'hsl(22, 97%, 66%)',
   neutralMain: 'hsl(219, 25%, 14%)',
   neutralPrimaryMVP: 'hsl(220, 26%, 14%)',
   neutralDark: 'hsl(195, 8%, 38%)',
@@ -25,6 +26,12 @@ export default {
   gray9: 'hsl(224, 11%, 26%)',
   primaryMainObj: primaryMain,
   secondaryMainObj: secondaryMain,
+  facebookBlue: 'hsla(214, 89%, 52%, 1)',
+  whatsappGreen: 'hsla(142, 70%, 49%, 1)',
+  irlenBlue: '#96ADFC',
+  irlenGreen: '#A8F29A',
+  irlenYellow: '#EDDD6E',
+  irlenRed: '#E0A6AA',
 };
 
 // HERE FOR REFERENCE ONLY
