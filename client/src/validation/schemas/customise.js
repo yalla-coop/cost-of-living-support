1ffb03debf7c57edc5e02567247f597ad51576c0
fadi <fadeomar2015@gmail.com>
2022-11-26 22:09:45 +0200

    update customise page validation

--- client/src/validation/schemas/customise.js
+++ client/src/validation/schemas/customise.js
@@ -3,9 +3,17 @@ import { fields, createSchema, validate as _validate } from '..';
 const { requiredText, hexColor } = fields;
 
 const schema = createSchema({
-  mainColor: hexColor,
-  secondaryColor: hexColor,
   logoFile: requiredText,
+  primaryBgMain: hexColor,
+  secondaryBgMain: hexColor,
+  tertiaryBgMain: hexColor,
+  quartenaryBgMain: hexColor,
+  quinaryBgMain: hexColor,
+  primaryTextMain: hexColor,
+  secondaryTextMain: hexColor,
+  tertiaryTextMain: hexColor,
+  quartenaryTextMain: hexColor,
+  quinaryTextMain: hexColor,
 });
 
 const validate = (data) => {
