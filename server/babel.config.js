3cc66af735b096708f1f5597810feae983615e68
Mark Claydon <mark@markclaydon.com>
2022-11-29 16:55:07 +0000

    Fixes for server build

--- server/babel.config.js
+++ server/babel.config.js
@@ -1,12 +1,25 @@
 module.exports = {
   presets: [
-    '@babel/preset-env'
+    '@babel/preset-env',
+    '@babel/preset-flow',
+    '@babel/preset-react'
   ],
+  sourceType: "unambiguous",
+  parserOpts: { allowReturnOutsideFunction: true },
   plugins: [
     ["@babel/plugin-transform-runtime",
       {
         regenerator: true
       }
     ]
+    /*
+    "babel-plugin-transform-remove-strict-mode"
+    ["babel-plugin-transform-not-strict",
+      {
+        regenerator: true,
+        removeAll: true
+      }
+    ]
+  */
   ]
 }
