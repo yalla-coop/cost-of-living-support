cbb2c8f80e3be1254343c7f0374589af2cb85590
fadi <fadeomar2015@gmail.com>
2022-11-29 13:31:58 +0200

    update Edit Content page

--- client/src/pages/Admin/EditContent/index.js
+++ client/src/pages/Admin/EditContent/index.js
@@ -1,52 +1,106 @@
+import { useState, useEffect } from 'react';
+import { Sections } from '../../../api-calls';
+import { message } from 'antd';
+import { useAdminOrg } from '../../../context/admin-org';
+
 import {
   Typography as T,
   Grid,
-  TextWithIcon,
-  Cards as C,
+  ButtonsSection,
+  Button,
 } from '../../../components';
-import { navRoutes as R } from '../../../constants';
+import { navRoutes } from '../../../constants';
+import { useNavigate } from 'react-router-dom';
 
 const { Col, Row } = Grid;
 
 const EditContent = () => {
+  const [buttons, setButtons] = useState([]);
+  const navigate = useNavigate();
+
+  const { adminOrg } = useAdminOrg();
+
+  useEffect(() => {
+    let mounted = true;
+    async function fetchData() {
+      const hideMessage = message.loading('Loading...');
+      const { data, error } = await Sections.getSections({
+        uniqueSlug: adminOrg.uniqueSlug,
+        forPublic: true,
+      });
+      if (mounted) {
+        if (error) {
+          message.error('Something went wrong, please try again later');
+        } else {
+          setButtons(data.map((item) => ({ ...item, id: item.id.toString() })));
+        }
+        hideMessage();
+      }
+    }
+
+    fetchData();
+    return () => {
+      mounted = false;
+    };
+  }, [adminOrg.uniqueSlug]);
+
+  const handleHide = (item) => {
+    setButtons((old) =>
+      old.map((elm) =>
+        elm.id === item.id ? { ...elm, hidden: !elm?.hidden } : elm
+      )
+    );
+  };
+
+  const handleEdit = (item) => {
+    navigate(navRoutes.ADMIN.EDIT_SECTION.replace('id', item.id));
+  };
+
+  const handleSaveChange = () => {
+    // send the updates to backend (order, and hide/show status)
+    const orderedItems = buttons.map((item, index) => ({
+      ...item,
+      order: index + 1,
+    }));
+  };
+
   return (
     <>
-      <Row jc="space-between" mb="7">
-        <Col w={[4, 12, 7]}>
-          <T.H1 mtM="5" style={{ width: '100%' }}>
-            Edit content
-          </T.H1>
+      <Row mb="6">
+        <Col w={[4, 12, 8]}>
+          <T.H1>Add/update content</T.H1>
         </Col>
       </Row>
-      <Row style={{ maxWidth: '700px' }}>
-        <Col w={[4, 12, 7]}>
-          <C.Tips
-            tips={[
-              <T.H3 color="secondaryMain">
-                Remember! This will change the main content of the tool, for
-                every organisation using it. Please double check any new
-                information and always keep the content style supportive. Read
-                out content guide here
-              </T.H3>,
-            ]}
-            startingColor={1}
+      <Row mb="5">
+        <Col w={[4, 12, 8]}>
+          <T.P color="neutralDark">
+            Drag the sections to reorder the content that your clients see or
+            click you can hide any sections that isn't relevant to your
+            organisation.
+          </T.P>
+        </Col>
+      </Row>
+      <Row>
+        <Col w={[4, 12, 8]}>
+          <ButtonsSection
+            setButtons={setButtons}
+            buttons={buttons}
+            handleHide={handleHide}
+            handleEdit={handleEdit}
+            m="2"
           />
         </Col>
-        <Col w={[4, 12, 5]} style={{ paddingLeft: '30px' }}>
-          <TextWithIcon
-            to={R.SUPER_ADMIN.EDIT_LANDING_PAGE}
-            text="Edit landing page content"
-            icon="forwardArrow"
-            iconColor="primaryMain"
-            m={{ mtT: 7 }}
-          />
-          <TextWithIcon
-            to={R.SUPER_ADMIN.MANAGE_STEPS}
-            text="Manage steps "
-            icon="forwardArrow"
-            iconColor="primaryMain"
-            m={{ mt: 4 }}
-          />
+      </Row>
+      <Row mb="6" mt="5">
+        <Col w={[4, 10, 5]}>
+          <Button variant="secondary" to={navRoutes.ADMIN.ADD_NEW_SECTION}>
+            Add new section
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col w={[4, 10, 5]}>
+          <Button handleClick={handleSaveChange}>Save changes</Button>
         </Col>
       </Row>
     </>
