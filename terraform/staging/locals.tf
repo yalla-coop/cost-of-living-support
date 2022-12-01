915fc6b10c4500132dc0c6a546b96f4044e5a37c
Mark Claydon <mark@markclaydon.com>
2022-12-01 12:22:41 +0000

    Updated local variables in Terraform

--- terraform/staging/locals.tf
+++ terraform/staging/locals.tf
@@ -2,7 +2,7 @@ locals {
   # active region
   aws_region = "${var.aws_region}"
   # The name of the S3 bucket
-  aws_bucket_name = "${var.aws_resource_prefix}-${var.aws_resource_prefix}"
+  aws_bucket_name = "${var.aws_resource_prefix}-${var.environment}"
   # The name of the S3 bucket user
-  aws_bucket_user_name = "${var.aws_resource_prefix}-${var.aws_resource_prefix}-s3"
+  aws_bucket_user_name = "${var.aws_resource_prefix}-${var.environment}-s3"
 }
