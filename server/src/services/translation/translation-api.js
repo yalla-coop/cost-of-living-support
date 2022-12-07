96ff3268e7adb635ceb351f0edc9d32dd1d5344f
Simon Dupree <dupreenator@gmail.com>
2022-12-07 10:43:19 +0100

    add more console logs and error handling

--- server/src/services/translation/translation-api.js
+++ server/src/services/translation/translation-api.js
@@ -15,35 +15,44 @@ const translate = async ({ source, target, json, id }) => {
     throw new Error('translation api: missing parameters');
   }
   const AWSTranslateJSONInst = new AWSTranslateJSON(awsConfig, source, target);
-
-  const translateJSON = async (_retries) => {
-    try {
-      const value = await AWSTranslateJSONInst.translateJSON(
-        removeNullsAndEmptyArraysAndObjects(json),
-      );
-      return {
-        id,
-        content: { ...(value[target] || value[target[0]]) },
-        languageCode: target[0],
-      };
-    } catch (error) {
-      console.log('translation api reached catch with retry:>> ', _retries);
-      if (_retries > 3) {
-        throw new Error('translation api catch error: ', error);
-      } else {
-        return translateJSON(_retries + 1);
+  if (AWSTranslateJSONInst && AWSTranslateJSONInst.translateJSON) {
+    console.log(
+      'object :>> ',
+      AWSTranslateJSONInst.translateJSON.TranslatedText,
+    );
+    const translateJSON = async (_retries) => {
+      try {
+        const value = await AWSTranslateJSONInst.translateJSON(
+          removeNullsAndEmptyArraysAndObjects(json),
+        );
+        return {
+          id,
+          content: { ...(value[target] || value[target[0]]) },
+          languageCode: target[0],
+        };
+      } catch (error) {
+        console.log('translation api reached catch with retry:>> ', _retries);
+        if (_retries > 3) {
+          throw new Error('translation api catch error: ', error);
+        } else {
+          return translateJSON(_retries + 1);
+        }
       }
-    }
-  };
+    };
 
-  try {
-    const res = await translateJSON(0);
-    if (res) {
-      return res;
+    try {
+      const res = await translateJSON(0);
+      if (res) {
+        return res;
+      }
+      throw new Error('translation api: try statement error');
+    } catch (error) {
+      throw new Error('translation api error: ', error);
     }
-    throw new Error();
-  } catch (error) {
-    throw new Error('translation api error: ', error);
+  } else {
+    throw new Error(
+      'translation api error: no AWS translation service initiated',
+    );
   }
 };
 
