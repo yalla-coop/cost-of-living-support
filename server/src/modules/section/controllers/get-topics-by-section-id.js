e1fffd4c319affb400b26eb4261b9532d4460a33
Ramy Shurafa <ramyshurafa@hotmail.com>
2023-11-21 15:41:06 +0300

    fix missing language param (#302)

--- server/src/modules/section/controllers/get-topics-by-section-id.js
+++ server/src/modules/section/controllers/get-topics-by-section-id.js
@@ -3,7 +3,7 @@ import * as Sections from '../use-cases';
 const getTopicsBySectionId = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { lng, forPublic } = req.query;
+    const { lng = 'en', forPublic } = req.query;
 
     const topics = await Sections.getTopicsBySectionId({
       id,
